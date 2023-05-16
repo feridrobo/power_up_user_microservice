4890f29d8f4a580cd57a1a228e1aa3da70d0ff27
Manuel Idrobo <feridrobo3@gmail.com>
2023-05-16 00:07:03 -0500

    feat(user): Verificación de existencia por DNI

--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driven/jpa/mysql/adapter/UserMysqlAdapter.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driven/jpa/mysql/adapter/UserMysqlAdapter.java
@@ -2,6 +2,7 @@ package com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.adapter;
 
 import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.exceptions.MailAlreadyExistsException;
 import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.exceptions.PersonAlreadyExistsException;
+import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.exceptions.UserAlreadyExistsException;
 import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.mappers.IUserEntityMapper;
 import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.repositories.IUserRepository;
 import com.pragma.powerup.usermicroservice.domain.model.User;
@@ -11,20 +12,23 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @RequiredArgsConstructor
 public class UserMysqlAdapter implements IUserPersistencePort {
-    private final IUserRepository personRepository;
+    private final IUserRepository userRepository;
     private final IUserEntityMapper personEntityMapper;
     private final PasswordEncoder passwordEncoder;
     @Override
     public void saveUser(User user) {
-        if (personRepository.findByDniNumber(user.getDniNumber()).isPresent()) {
+        if (userRepository.existsByDniNumber(user.getDniNumber())) {
+            throw new UserAlreadyExistsException();
+        }
+        if (userRepository.findByDniNumber(user.getDniNumber()).isPresent()) {
             throw new PersonAlreadyExistsException();
         }
 
-        if (personRepository.existsByMail(user.getMail())){
+        if (userRepository.existsByMail(user.getMail())){
             throw new MailAlreadyExistsException();
         }
 
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        personRepository.save(personEntityMapper.toEntity(user));
+        userRepository.save(personEntityMapper.toEntity(user));
     }
 }
